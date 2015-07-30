0c62815b10f23dc4b1c532d73237c74b76e04e6f
kma <kma@ncsu.edu>
2015-07-29 21:21:29 -0400

    Missio for Friday

--- Database_Exportation/src/dao/inserter/CourseInserter.java
+++ Database_Exportation/src/dao/inserter/CourseInserter.java
@@ -21,10 +21,9 @@ public class CourseInserter
 		   )
 		   {
 				pstmt.clearParameters();
-			    //executeUpdate 执行插入语句
 				pstmt.setString(1, course.getCourseID().toString());
 				pstmt.setString(2, null);
-				pstmt.setObject(3, course.getCourseTitle()); //如果不知道占位符是什么类型的数据就用setObject，jdbc会自动为您转成合适的类型
+				pstmt.setObject(3, course.getCourseTitle()); 
 				pstmt.setObject(4, course.getCourseDescription()); 
 				pstmt.setObject(5, null); 
 				pstmt.setObject(6, course.getCourseCreated()); 
@@ -34,13 +33,10 @@ public class CourseInserter
 				pstmt.executeUpdate();
 				System.out.println("=======================");
 				
-				//clearParameters：清理当前的参数，如果直接执行会带来异常:No value specified for parameter 1
-				//pstmt.clearParameters();
-				//pstmt.executeUpdate(); 
 				
 		   }catch(Exception e)
 		   {e.printStackTrace();
-			   System.out.println("数据库操作出现异常");
+			   System.out.println("Database error");
 		   }
 	}
 
---
+++ Database_Exportation/src/dao/inserter/TaskInserter.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package dao.inserter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import model.Task;
+
+/**
+ * A class for inserting tasks into PRML database
+ * @author kma
+ *
+ */
+public class TaskInserter {
+	
+	public static void insertSingle(Task task) {
+		try(
+				Connection con = DriverManager.getConnection("jdbc:mysql://localhost/PRML", "root", "");
+				PreparedStatement pstmt = con.prepareStatement("Insert into Task (TaskID, TaskTypeID, TaskTitle, TaskPrompt, TaskOpen, TaskDue) values (?,?,?,?,?,?)");
+		   )
+		   {
+				pstmt.clearParameters();
+				pstmt.setString(1, task.getTaskID().toString());
+				pstmt.setString(2, task.getTaskTypeID().toString());
+				pstmt.setObject(3, task.getTaskTitle()); 
+				pstmt.setObject(4, task.getTaskPrompt()); 
+				pstmt.setObject(5, null); 
+				pstmt.setObject(6, task.getTaskDue()); 
+				
+				pstmt.executeUpdate();
+				System.out.println("==========Task object inserted=============");
+				
+				
+		   }catch(Exception e)
+		   {e.printStackTrace();
+			   System.out.println("Database error");
+		   }
+		
+	}
+}
